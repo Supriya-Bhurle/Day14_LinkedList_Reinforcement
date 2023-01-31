17dd2382f560a8c5473cb4a96f0f6672019a7797
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-31 10:43:32 +0530

    Added : create link list and insert element in the list

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -13,5 +13,16 @@ public class LinkedListMain {
         linkedList2.append(30);
         linkedList2.append(70);
         linkedList2.show();
+
+
+        LinkedList<Integer> linkedList3=new LinkedList<>();
+        System.out.println("\n******** : Insert a node in between : *********");
+        linkedList3.append(56);
+        linkedList3.append(70);
+        System.out.println("********** : Display linked list without insert : *********");
+        linkedList3.show(); // Display Node elements
+        System.out.println("******** : Display linked list insert a node in between : *******");
+        linkedList3.searchAndAdd(56,30);
+        linkedList3.show(); // Display Node elements
     }
 }
