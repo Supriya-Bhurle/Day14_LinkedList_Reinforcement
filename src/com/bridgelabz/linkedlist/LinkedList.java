b139d5e2929aa7ab6738e91c637b943604f72726
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-01-31 09:27:16 +0530

    Added : Create linked list by adding

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,16 +1,26 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList <T>{ Node<T> head;
+public class LinkedList <T>{
+    Node<T> head;
     Node<T> tail;
-    public void push(T key){
-        Node<T> newNode = new Node<>();
-        if (head == null){
+    public void push(T key) {
+        Node<T> newNode=new Node<>(key);
+        if (head==null){
             head=newNode;
-            tail= newNode;
-        }else {
-            newNode.next = head;
-            head = newNode;
+            tail=newNode;
         }
+        else {
+            newNode.next=head;
+            head=newNode;
+        }
+    }
 
+    public void show() {
+        Node<T> temp=head;
+        while (temp!=null){
+            System.out.print(temp.key+"=>");
+            temp=temp.next;
+        }
+        System.out.println();
     }
 }
--- src/com/bridgelabz/linkedlist/Node.java
+++ src/com/bridgelabz/linkedlist/Node.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.linkedlist;
 
-public class Node <T>{ T key;
+public class Node <T>{
+    T key;
     Node<T> next;
 
-    public Node() {
 
-    }
 
     public Node(T key ) {
         this.key = key;
